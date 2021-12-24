1f3aadb11d39b4447b1e07eb57eac1b8636b8081
bikash-tsd <bdash@tsdweb.com>
2021-12-24 17:48:56 +0530

    trupls

--- app.ts
+++ app.ts
@@ -1,14 +1,17 @@
-const person:{
+let person:{
   name:string,
   age:number,
+  role:[number,string]
+  
 } = {
   name:"Maximilian",
-  age:29
+  age:29,
+  role:[2,'author']
 }
 let foveateActivate:string[]; 
 foveateActivate =['football']; 
-
-for (const hobby of foveateActivate) {
-  console.log(hobby.toLocaleUpperCase());
-  
+person = {
+  name:"re",
+  age:21,
+  role:[2,'333']
 }
