fde49d98721825a13b5cad755a6dbf61f692a967
bikash-tsd <bdash@tsdweb.com>
2021-12-24 21:40:54 +0530

    tsconfig file

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-function addNumber(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log(num);
-}
-function addHandlebar(num1, num2, cb) {
-    var result = num1 + num2;
-    return cb(result);
-}
-addHandlebar(3, 4, printResult);
+var userInput;
+var userName;
+userName = '54';
--- app.ts
+++ app.ts
@@ -1,14 +1,4 @@
-function addNumber(n1: number, n2: number) {
-  return n1 + n2;
-}
+let userInput : unknown;
+let userName : string;
 
-function printResult(num: number) {
-  console.log(num);
-}
-
-function addHandlebar(num1: number, num2: number, cb: (a: number) => void) {
-  const result = num1 + num2;
-  return cb(result);
-}
-
-addHandlebar(3, 4, printResult);
+userName='54'
