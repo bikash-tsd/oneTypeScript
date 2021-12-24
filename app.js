fb6e3f777f54be1fa04a0c5e2f4c47be15f26a07
bikash-tsd <bdash@tsdweb.com>
2021-12-24 18:44:30 +0530

    function type

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-function combine(input1, input2, addType) {
-    var result;
-    if (addType === 'on-text') {
-        result = input1.toString() + input2.toString();
-    }
-    if (addType === 'on-number') {
-        result = +input1 + +input2;
-    }
-    return result;
+function addNumber(n1, n2) {
+    return n1 + n2;
 }
+function printResult(num) {
+    console.log('result' + num);
+}
+printResult(addNumber(1, 3));
--- app.ts
+++ app.ts
@@ -1,19 +1,11 @@
-type Combinable =  number | string;
-type AddTypeAliases ='on-text'|'on-number';
-
-
-function combine(input1:Combinable , input2:Combinable, addType:AddTypeAliases) {
-  let result;
-  if(addType==='on-text'){
-    result = input1.toString()+ input2.toString()
-  }
-
-  if(addType==='on-number'){
-    result = +input1 + +input2
-  }
-  
-  
-  return result;
+function addNumber(n1:number,n2:number)  {
+  return n1+n2
 }
 
+function printResult(num:number) {
+  return 
+  
+}
 
+let combine : (a:number,b:number)=>number;
+combine =addNumber ; 
