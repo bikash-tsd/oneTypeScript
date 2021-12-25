a892de79b19c2916a01895d8162470ec1f3d7698
bikash-tsd <bdash@tsdweb.com>
2021-12-25 11:42:55 +0530

    modifier

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,27 @@
-const add = (a:number=1,b:number=1)=>{
-  return a+b
-}
-
-const printOutPut = (a:number|string):void=>{
-  console.log(a);
-  
-}
-
-printOutPut(add(2));
-
-const hobby = ['sport','cooking'];
-const activeHobby = ['hiking'];
-activeHobby.push(...hobby);
-console.log(activeHobby);
-
-const person = {
-  name:'bikash',
-  age:29
-}
-const copyPerson = {...person};
-console.log(copyPerson);
-
-const addNumber = (...numbers:number[])=>{
-  const sum =numbers.reduce((currentValue,reduceValue)=>{
-    return currentValue+reduceValue
-  },0);
-  return sum
-  
+class Department {
+  private name :string;
+  private employee:string[];
+  constructor(n:string) {
+    this.name = n;
+    this.employee =[];
+  }
+  describe(){
+    console.log('department name: '+this.name);
+    
+  }
+  addEmployee(empName:string){
+    this.employee.push(empName)
+  }
+  printEmployee(){
+    console.log(this.employee.length,this.employee);
+    
+  }
 
 }
 
-console.log(addNumber(1,2,3,4));
+const dep = new Department('accounting');
+
+dep.addEmployee('max');
+dep.addEmployee('dash');
+dep.printEmployee();
 
